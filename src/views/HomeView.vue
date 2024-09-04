0fc007a6bdc6b3390d7afff370a7c3b8cd566494
Гарипов Нияз <20arsik2015@mail.ru>
2024-09-04 20:59:47 +0300

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class=""></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },
+  props: {
+  },
+  setup () {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
